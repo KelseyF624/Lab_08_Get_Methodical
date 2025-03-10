5c87bce609a5b182407d1cd7ea8b69da7c834233
KelseyF624 <kelseyf.624@gmail.com>
2025-03-10 12:51:17 -0400

    Partial Commit

--- src/CtoFTableDisplay.java
+++ src/CtoFTableDisplay.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class CtoFTableDisplay {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double celciusTemp = 0;
+        double fahrenheitTemp = 0;
+        int celciusStart = 0;
+        int celciusEnd = 0;
+        double rangeCelcius = 0;
+        celciusStart = SafeInput.getInt(input, "Enter celcius temperature.");
+        celciusEnd = SafeInput.getInt(input, "Enter the celcius temperature you would like to end at.");
+        rangeCelcius = celciusEnd - celciusStart;
+        celciusTemp = celciusStart;
+        System.out.println("Celcius \t\tFahrenheit");
+        for (int i = 1; i <= rangeCelcius +1; i++) {
+            fahrenheitTemp
+        };
+    }
 }
