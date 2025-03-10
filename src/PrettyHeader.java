28437e87c2d80bc975bbb3b667cadee6578b1615
KelseyF624 <kelseyf.624@gmail.com>
2025-03-10 15:04:39 -0400

    Partial Commit

--- src/PrettyHeader.java
+++ src/PrettyHeader.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 public class PrettyHeader {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        String message = SafeInput.getNonZeroLenString(input, "Enter a message to display.");
+        prettyHeader(message);}
+    public static void prettyHeader(String message) {
+        int spaces = 54 - message.length();
+        int space1 = spaces / 2;
+        int space2 = space1;
+        if (spaces %2 == 1)
+            space2++;
+        for (int s = 1; s <= spaces; s++) {
+            System.out.print("*");}
+        for (int sp = 1; sp <= spaces; sp++) {
+            System.out.print(" ");}
+        for (int s = 1; s <= 3; s++) {
+            System.out.print("*");}
+        for (int s = 1; s <= 60; s++) {
+            System.out.print("*");}
     }
 }
