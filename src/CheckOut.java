aba1da71de24294b7be99578517c3cbd5d4c2f95
KelseyF624 <kelseyf.624@gmail.com>
2025-03-10 22:57:02 -0400

    Partial Commit

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -4,12 +4,14 @@ public class CheckOut {
         Scanner input = new Scanner(System.in);
         double itemPrice = 0;
         double totalPrice = 0;
-        boolean moreItems = true;
+        int itemCount = 0;
+        boolean done = false;
         do {
-            itemPrice = SafeInput.getRangedDouble(input, "Enter the item price.", .50,10.00);
+            itemPrice = SafeInput.getRangedDouble(input,"Enter the item price.",.50,10.00);
             totalPrice += itemPrice;
-            moreItems = SafeInput.getYNConfirm(input, "Do you have anymore items? (y/n)");
-        }while (moreItems);
-        System.out.print("The total price is $ %.2f." + totalPrice);
+            itemCount++;
+            done = SafeInput.getYNConfirm(input, "Are you done? (y/n)");
+        }while (!done);
+        System.out.print("The total of these " + itemCount + " items is " + totalPrice);
     }
 }
